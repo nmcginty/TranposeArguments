810bca922f9ba684fad401c993ddfbdc5ed197dd
Nick McGinty <nmcginty@lce.com>
2017-09-26 18:57:49 -0400

    trans, revtrans & random all work for multiple selections; selectAll still in the works

--- transposePlugin.py
+++ transposePlugin.py
@@ -1,19 +1,21 @@
-import sublime, sublime_plugin, random
+import sublime, sublime_plugin, random, re
 # from subprocess import call
 
 class TransposeArgsCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-
 		self.view.run_command("expand_selection" , {"to": "brackets"})
-		sel = self.view.sel()[0]
-		tranposedText = self.tranposeArg(self.view.substr(sel))
+		selection = self.view.sel() #list of all current selections / cursors
+		
+		for sel in selection:
+			if(not sel.empty()):
+				tranposedText = self.tranposeArg(self.view.substr(sel))
+				self.view.replace(edit, sel, tranposedText)
 
-		if(not sel.empty()):
-			self.view.run_command("left_delete")
-			self.view.insert(edit, self.view.sel()[0].begin(), tranposedText)
+			else:
+				self.view.replace(edit, sel, "run command within ()")
 
-		else:
-			self.view.insert(edit, self.view.sel()[0].begin(), "run command within ()")
+		#sets cursor(s) back to beginning	
+		self.view.run_command("move" , {"by": "characters", "forward": False})
 
 	@staticmethod
 	def tranposeArg(selectionText):
@@ -35,15 +37,18 @@ class TransposeArgsCommand(sublime_plugin.TextCommand):
 class ReverseTransposeArgsCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.view.run_command("expand_selection" , {"to": "brackets"})
-		sel = self.view.sel()[0]
-		tranposedText = self.reverseTranposeArg(self.view.substr(sel))
+		selection = self.view.sel() #list of all current selections / cursors
+		
+		for sel in selection:
+			if(not sel.empty()):
+				tranposedText = self.reverseTranposeArg(self.view.substr(sel))
+				self.view.replace(edit, sel, tranposedText)
 
-		if(not sel.empty()):
-			self.view.run_command("left_delete")
-			self.view.insert(edit, sel.begin(), tranposedText)
+			else:
+				self.view.replace(edit, sel, "run command within ()")
 
-		else:
-			self.view.insert(edit, sel.begin(), "run command within ()")
+		#sets cursor(s) back to beginning	
+		self.view.run_command("move" , {"by": "characters", "forward": False})
 
 
 	@staticmethod
@@ -66,18 +71,21 @@ class ReverseTransposeArgsCommand(sublime_plugin.TextCommand):
 class RandTransposeCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.view.run_command("expand_selection" , {"to": "brackets"})
-		sel = self.view.sel()[0]
-		tranposedText = self.randTranspose(self.view.substr(sel))
+		selection = self.view.sel() #list of all current selections / cursors
+		
+		for sel in selection:
+			if(not sel.empty()):
+				tranposedText = self.randTransposeArg(self.view.substr(sel))
+				self.view.replace(edit, sel, tranposedText)
 
-		if(not sel.empty()):
-			self.view.run_command("left_delete")
-			self.view.insert(edit, sel.begin(), tranposedText)
+			else:
+				self.view.replace(edit, sel, "run command within ()")
 
-		else:
-			self.view.insert(edit, sel.begin(), "run command within ()")
+		#sets cursor(s) back to beginning	
+		self.view.run_command("move" , {"by": "characters", "forward": False})
 
 	@staticmethod
-	def randTranspose(selectionText):
+	def randTransposeArg(selectionText):
 		#still have to fix this
 		if("," not in selectionText): #no space means argument < 2 
 			return selectionText
@@ -92,7 +100,76 @@ class RandTransposeCommand(sublime_plugin.TextCommand):
 		print("final jawn", "(", ', '.join(args), ")")
 		return ', '.join(args)
 
+class SelectAllArgsCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		self.view.run_command("expand_selection" , {"to": "brackets"})
+		sel = self.view.sel()[0] #string of arguments
+		formattedText = self.formatArgs(self.view.substr(sel))
+		count = formattedText.count(',')
 
+		if(not sel.empty()):
+			self.view.run_command("left_delete")
+			self.view.insert(edit, sel.begin(), formattedText)
+			self.selectAllArgs(self, count)
+
+		else:
+			self.view.insert(edit, sel.begin(), "run command within ()")
+
+
+		# self.view.run_command("expand_selection" , {"to": "brackets"})
+		# selection = self.view.sel() #list of all current selections / cursors
+		
+		# for sel in selection:
+		# 	if(not sel.empty()):
+		# 		formattedText = self.formatArgs(self.view.substr(sel))
+		# 		count = formattedText.count(',')
+		# 		self.view.replace(edit, sel, formattedText)	
+		# 		# self.view.run_command("left_delete")
+		# 		# self.view.insert(edit, sel.begin(), formattedText)
+		# 		self.selectAllArgs(self, count)
+
+		# 	else:
+		# 		self.view.replace(edit, sel, "run command within ()")
+				
+		# #sets cursor(s) back to beginning	
+		# self.view.run_command("move" , {"by": "characters", "forward": False})
+
+
+	@staticmethod		
+	def formatArgs(selectionText):
+		#still have to fix this
+		if("," not in selectionText): #no space means argument < 2 
+			return selectionText
+
+		args = selectionText.split(",") #dumps all args into array
+		args = [arg.strip() for arg in args] #take away any white space
+
+		print("final jawn", "(", ', '.join(args), ")")
+		return ', '.join(args) #formatted string is return
+
+	@staticmethod
+	def selectAllArgs(self, count):
+
+		#to position the cursor at the beginning of first argument
+		self.view.run_command("move_to" , {"to": "brackets"})
+		# self.view.run_command("move_to" , {"to": "brackets"})
+		self.view.run_command("mark_and_move_do_it_all")
+
+		for x in range(count):
+			self.view.run_command("move" , {"by": "word_ends", "forward": True})
+			# while()
+			# 	self.view.run_command("move" , {"by": "word_ends", "forward": True})
+
+			self.view.run_command("move" , {"by": "characters", "forward": True})
+			self.view.run_command("move" , {"by": "characters", "forward": True})
+			self.view.run_command("mark_and_move_do_it_all")	
+
+		#to execute to final command and initiate the cursors visually
+		self.view.run_command("mark_and_move_do_it_all")	
+
+		#highlight each argument
+		self.view.run_command("find_under_expand")
+		# self.view.run_command("find_under_expand")
 
 		#must do this next
 		#work for multi-cursor
@@ -102,4 +179,18 @@ class RandTransposeCommand(sublime_plugin.TextCommand):
 		# go from space word1 word2 space
 		# also has to work for this like int x, double y
 		# ctrl alt t, show cycle through arguments left to right
+		# self.view.run_command("move_to" , {"to": "brackets"})
+		# self.view.run_command("move_to" , {"to": "brackets"})
+		# self.view.run_command("find_under_expand")
+
+
+		# sel = self.view.sel()[0]
+		# tranposedText = self.randTranspose(self.view.substr(sel))
+
+		# if(not sel.empty()):
+		# 	self.view.run_command("left_delete")
+		# 	self.view.insert(edit, sel.begin(), tranposedText)
+
+		# else:
+		# 	self.view.insert(edit, sel.begin(), "run command within ()")
 		# ctrl shift alt t, would go right to left
